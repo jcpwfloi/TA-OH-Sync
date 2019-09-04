2bda8a3ff0f9aa0c8e73ca0aa99ae13ad69099af
jcpwfloi <jcpwfloi@gmail.com>
2019-09-04 12:05:24 -0400

    [CourseCare] parsing calendar

--- CourseCare.js
+++ CourseCare.js
@@ -4,8 +4,6 @@ class CourseCare {
   constructor({ username, password }) {
     this.username = username;
     this.password = password;
-
-    this.login(console.log);
   }
 
   login(callback) {
@@ -25,11 +23,63 @@ class CourseCare {
     });
   }
 
-  combine(data) {
+  merge(a, b) {
+    a.start = a.start < b.start ? a.start : b.start;
+    a.end = a.end > b.end ? a.end : b.end;;
+    return a;
+  }
+
+  trim(t) {
+    var ans = [];
+
+    for (var i = 0; i < t.length;) {
+      var pivot = t[i];
+
+      ++ i;
+
+      if (i >= t.length) {
+        ans.push(pivot);
+        break;
+      }
+
+      while (this.intersect(pivot, t[i])) {
+        pivot = this.merge(pivot, t[i]);
+        ++ i;
+
+        if (i >= t.length) break;
+      }
+
+      ans.push(pivot);
+    }
+
+    return ans;
+  }
+
+  intersect(d1, d2) {
+    if (d1.start > d2.start) {
+      t = d1;
+      d1 = d2;
+      d2 = t;
+    }
+    return d1.end >= d2.start;
   }
 
   sync(data) {
-    console.log(data);
+    var table = {};
+    var ans = [];
+
+    data.map((d) => {
+      if (!table[d.location]) table[d.location] = [];
+      
+      table[d.location].push(d);
+    });
+
+    for (var loc in table) {
+      table[loc] = this.trim(table[loc]);
+      ans = ans.concat(table[loc]);
+    }
+
+    console.log(ans);
   }
 }
 
